bbe9ad661f0de0f3aba78e51b4be1bb5c07e4dcf
Cesar Vianna <cesar.vianna@serpro.gov.br>
2023-06-07 23:24:22 -0300

    Reordenação funcionando

--- javascript/app/repertorio/script.js
+++ javascript/app/repertorio/script.js
@@ -1,86 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
   const fileName = "dadosDeEntrada.csv";
   const reader = new FileReader();
-  const tabela = document.getElementById("tabela-dados");
+  const tabela = document.getElementById("sortable-table");
 
-  // Função para atualizar o arquivo CSV
-  function updateCSV() {
-    const tableRows = Array.from(document.querySelectorAll("#tabela-dados tr"));
-    const headers = tableRows.shift().querySelectorAll("th");
-    const rows = tableRows.map(row => row.querySelectorAll("td"));
-
-    const newContents = [];
-    newContents.push(Array.from(headers).map(th => th.textContent).join(";"));
-
-    rows.forEach(row => {
-      const rowData = Array.from(row).map(td => td.textContent);
-      newContents.push(rowData.join(";"));
-    });
-
-    const newCSV = newContents.join("\n");
-    
-    fetch(fileName, {
-      method: "PUT", // Você pode ajustar o método HTTP conforme necessário (exemplo: PUT, POST)
-      body: newCSV
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("Failed to update CSV file");
-      }
-      console.log("CSV file updated successfully");
-    })
-    .catch(error => {
-      console.error(error);
-    });
-  }
-
-  // Manipuladores de eventos para arrastar e soltar
-  let draggingElement = null;
-
-  function handleDragStart(event) {
-    draggingElement = event.target;
-    event.dataTransfer.effectAllowed = "move";
-    event.dataTransfer.setData("text/html", event.target.outerHTML);
-    event.target.classList.add("dragging");
-  }
-
-  function handleDragEnter(event) {
-    event.target.classList.add("drag-over");
-  }
-
-  function handleDragLeave(event) {
-    event.target.classList.remove("drag-over");
-  }
-
-  function handleDragOver(event) {
-    event.preventDefault();
-    event.dataTransfer.dropEffect = "move";
-  }
-
-  function handleDrop(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    event.target.classList.remove("drag-over");
-
-    if (draggingElement !== event.target) {
-      draggingElement.outerHTML = event.target.outerHTML;
-      event.target.outerHTML = event.dataTransfer.getData("text/html");
-      updateCSV();
-    }
-  }
-
-  function handleDragEnd(event) {
-    draggingElement.classList.remove("dragging");
-    draggingElement = null;
-  }
-
-  // Adicionando manipuladores de eventos após o carregamento do DOM
-  tabela.addEventListener("dragstart", handleDragStart);
-  tabela.addEventListener("dragenter", handleDragEnter);
-  tabela.addEventListener("dragleave", handleDragLeave);
-  tabela.addEventListener("dragover", handleDragOver);
-  tabela.addEventListener("drop", handleDrop);
-  tabela.addEventListener("dragend", handleDragEnd);
 
   // Lendo o arquivo CSV
   fetch(fileName)
@@ -114,6 +36,8 @@ document.addEventListener("DOMContentLoaded", function() {
       for (let i = 1; i < rows.length; i++) {
         const rowData = rows[i].split(";");
         const row = document.createElement("tr");
+        row.classList = "drag-handle";
+        
 
         for (let j = 0; j < rowData.length; j++) {
           const td = document.createElement("td");
@@ -124,5 +48,84 @@ document.addEventListener("DOMContentLoaded", function() {
         tabela.appendChild(row);
       }
     }
+    enableDragAndDrop();
   };
+  
+//  Drag and Drop
+function enableDragAndDrop() {
+  const rows = document.querySelectorAll('#sortable-table tr');
+  rows.forEach((row, index) => {
+    // Verifica se o elemento tr não tem um elemento th como filho direto
+    if (!row.querySelector('th') || !row.querySelector('th').parentNode.isSameNode(row)) {
+      row.draggable = true;
+      row.dataset.index = index; // Adiciona o atributo data-index para manter o índice original
+
+      row.addEventListener('dragstart', (e) => {
+        e.dataTransfer.setData('text/plain', row.dataset.index); // Armazena o índice original
+        e.dataTransfer.effectAllowed = 'move';
+        row.classList.add('dragging');
+      });
+
+      row.addEventListener('dragend', () => {
+        row.classList.remove('dragging');
+        updateTableOrder();
+        // updateCSV();
+      });
+
+      row.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        const draggingRow = document.querySelector('.dragging');
+
+        if (draggingRow && draggingRow !== row) {
+          const draggingIndex = parseInt(draggingRow.dataset.index);
+          const targetIndex = parseInt(row.dataset.index);
+
+          if (draggingIndex < targetIndex) {
+            row.parentNode.insertBefore(draggingRow, row.nextSibling);
+          } else {
+            row.parentNode.insertBefore(draggingRow, row);
+          }
+          updateTableOrder();
+        }
+      });
+    }
+  });
+}
+
+
+function updateTableOrder() {
+  const table = document.getElementById('sortable-table');
+  const tbody = table.querySelector('tbody');
+  
+  // Verifica se o tbody já foi removido
+  if (!tbody) return;
+
+  const rows = tbody.querySelectorAll('tr');
+  const newTbody = document.createElement('tbody');
+
+  rows.forEach((row, index) => {
+    const newRow = document.createElement('tr');
+    const cells = row.querySelectorAll('td');
+
+    cells.forEach(cell => {
+      const newCell = document.createElement('td');
+      newCell.textContent = cell.textContent;
+      newRow.appendChild(newCell);
+    });
+
+    newTbody.appendChild(newRow);
+  });
+
+  // Limpa as linhas existentes
+  while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+  }
+
+  // Adiciona as novas linhas reordenadas
+  table.appendChild(newTbody);
+}
+
+
+
+  
 });
